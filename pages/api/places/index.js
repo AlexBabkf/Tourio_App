16ccee2045eb468c8cee4470c937dbdb4e92d6d9
Georgios Alexandros Christodoulou <bab_alt@hotmail.com>
2023-06-13 10:22:46 +0200

    just for vercel

---
+++ pages/api/places/index.js
@@ -0,0 +1,23 @@
+import dbConnect from "../../../db/connect";
+import Place from "../../../db/models/Place";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const places = await Place.find();
+    return response.status(200).json(places);
+  }
+
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      const place = new Place(placeData);
+      await place.save();
+      return response.status(201).json({ status: "place created." });
+    } catch (error) {
+      console.error(error);
+      return response.status(400).json({ error: error.message });
+    }
+  }
+}
---
+++ pages/places/[id]/edit.js
@@ -0,0 +1,41 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+import useSWR from "swr";
+import Form from "../../../components/Form.js";
+import { StyledLink } from "../../../components/StyledLink.js";
+import useSWRMutation from "swr/mutation";
+
+export default function EditPage() {
+  const router = useRouter();
+  const { isReady, push } = router;
+  const { id } = router.query;
+  const { data: place, isLoading, error } = useSWR(`/api/places/${id}`);
+
+  async function editPlace(place) {
+    const response = await fetch(`/api/places/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify(place),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      await response.json();
+    } else {
+      console.error(`Error: ${response.status}`);
+    }
+  }
+
+  if (!isReady || isLoading || error) return <h2>Loading...</h2>;
+
+  return (
+    <>
+      <h2 id="edit-place">Edit Place</h2>
+      <Link href={`/places/${id}`} passHref legacyBehavior>
+        <StyledLink justifySelf="start">back</StyledLink>
+      </Link>
+      <Form onSubmit={editPlace} formName={"edit-place"} defaultData={place} />
+    </>
+  );
+}
